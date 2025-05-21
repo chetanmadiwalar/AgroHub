7ed5221b1a9a014c6459a045ef1ca04621d2522b
chetanmadiwalar <chetanmadiwalar1947@gmail.com>
2025-05-21 19:21:07 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -43,16 +43,21 @@ const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 app.use('/img', express.static(path.join(process.cwd(), 'public', 'img')));
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')))
-
-    app.get('*', (req, res) =>
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
-} else {
-    app.get('/', (req, res) => {
+app.get('/', (req, res) => {
         res.send("API is running");
     })
-}
+
+
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+//     app.get('*', (req, res) =>
+//         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+// } else {
+//     app.get('/', (req, res) => {
+//         res.send("API is running");
+//     })
+// }
 
 app.use(notFound)
 app.use(errorHandler)
