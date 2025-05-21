11bd51d150da9b421e86bea428f43f553390490a
chetanmadiwalar <chetanmadiwalar1947@gmail.com>
2025-05-21 19:25:45 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -64,7 +64,9 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
-    PORT,
-    console.log(`Server running ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold)
-);
+// app.listen(
+//     PORT,
+//     console.log(`Server running ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold)
+// );
+
+module.exports = app;
