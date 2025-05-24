ddb4de08debb16710ffb71dd1cc3f484405ce174
chetanmadiwalar <chetanmadiwalar1947@gmail.com>
2025-05-24 12:21:22 +0530

    updatd server.js

--- backend/server.js
+++ backend/server.js
@@ -61,9 +61,9 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.use('/img', express.static(path.join(process.cwd(), 'public', 'img')));
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
+  app.use(express.static(path.join(__dirname, '../frontend/build')));
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '../frontend', 'build', 'index.html'))
   );
 } else {
   app.get('/', (req, res) => res.send('API is running'));
